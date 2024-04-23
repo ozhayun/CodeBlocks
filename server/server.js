f9b40e82fd0265c3ed5a9d8bf7bc9b096cf0664b
Oz Hayun <ozhayun2@gmail.com>
2024-04-23 11:06:08 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
